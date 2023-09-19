2b94e75614eba8635cd061b661938a0643829bd8
pmtay <pmtay@tma.com.vn>
2023-09-19 22:02:01 +0700

    dd

---
+++ personal/mainIndex/main.js
@@ -0,0 +1,20 @@
+var typed = new Typed(".text", {
+    strings:["I can Swim", " I can Fly" , "I can Run"],
+    typeSpeed :20,
+    backSpeed : 50,
+    backDelay : 1000,
+    loop :true
+});
+
+// const btn = document.getElementById('submit');
+
+// btn.addEventListener('click', function handleClick(event) {
+//   // 👇️ if you are submitting a form
+//   event.preventDefault();
+
+//   const inputs = document.querySelectorAll('#IDname, #IDmessage');
+
+//   inputs.forEach(input => {
+//     input.value = '';
+//   });
+// });
