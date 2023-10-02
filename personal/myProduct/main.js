7054c684df816e61268b043301a93d09e744ae24
pmtay <pmtay@tma.com.vn>
2023-10-02 20:53:04 +0700

    m

--- personal/myProduct/main.js
+++ personal/myProduct/main.js
@@ -24,9 +24,52 @@ function ready(){
         var button = removeCartButton[i]
         button.addEventListener('click', removeCartItem)
     }
-
+    var quantityInput = document.getElementsByClassName('cart_quantity')
+    for (var i = 0; i< quantityInput.length; i++){
+        var input = quantityInput[i]
+        input.addEventListener("change", quantityChanged);
+    }
+    //add to cart
+    var addCart = document.getElementsByClassName('bx-cart')
+    for (var i = 0; i< addCart.length; i++){
+        var button = addCart[i]
+        button.addEventListener("click", addCartClicked)
+    }
 }
 function removeCartItem(event){
     var buttonClicked = event.target;
     buttonClicked.parentElement.remove();
+    updatetotal()
+}
+function quantityChanged(event){
+    var input  = event.target
+    if(isNaN(input.value) || input.value <=0){
+        input.value =1
+    }
+    updatetotal()
+}
+// ADd to cart
+function addCartClicked(event){
+    var button = event.target
+    var shopProduct = button.closest(".card")
+    var title =  shopProduct.getElementsByClassName('card_title')[0].innerText
+    // var price = shopProduct.getElementsByClassName("card_price")[0].innerText
+    console.log(title)
+}
+
+function updatetotal(){
+    var cartContent= document.getElementsByClassName('cart_content')[0]
+    var cartBoxed = cartContent.getElementsByClassName('cart_box')
+    total = 0
+    for (var i =0; i<cartBoxed.length; i++){
+        var cartBox =cartBoxed[i]
+        var priceElement = cartBox.getElementsByClassName('cart_price')[0]
+        var quantityElement = cartBox.getElementsByClassName('cart_quantity')[0]
+        var price  = parseFloat(priceElement.innerText.replace("$", ""))
+        var quantity =  quantityElement.value
+        total = total + (price * quantity)
+
+        document.getElementsByClassName('total_price')[0].innerText="$" + total
+
+    }
 }
